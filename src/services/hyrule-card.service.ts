116e8a5bdac14443c8dae83243c26cd0fcb3fa36
Aitana Sanchez <aitanabenrui2001@gmail.com>
2025-04-23 19:08:08 +0200

    feat: add async function for hyrule-cards

--- src/services/hyrule-card.service.ts
+++ src/services/hyrule-card.service.ts
@@ -1,9 +1,28 @@
+import { HyruleCardType, HyruleDataType } from "../types/hyrule.types"
 
 
-const getAllHyruleData = () => {
-    
-}
+const getAllHyruleData = async() : Promise<HyruleDataType[] | undefined> => {
+    const url = 'https://botw-compendium.herokuapp.com/api/v3/compendium/all';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        return response.data
+    } catch (error: any) {
+        throw new Error('Oops! Something went wrong while fetching data from the Hyrule Compendium');
+    }
+};
 
-export const getRandomHyruleData = () =>{
+export const getRandomHyruleData = async( num : number = 3 ) : Promise< HyruleDataType[] | undefined> => {
+    const allData = await getAllHyruleData();
+    if (!allData) return;
 
-}
+    // Mezclamos los datos con el algoritmo de Fisher-Yates
+    const shuffled = [...allData].sort(() => 0.5 - Math.random());
+
+     // Seleccionamos los primeros `num` elementos
+    const selected = shuffled.slice(0, num);
+
+    return selected;
+} 
