24905d3002980b0b67bdca58e447fe071b3eaa26
Juan <juansobraoperez@gmail.com>
2025-04-24 20:45:56 +0200

    feature: component button link

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -11,6 +11,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string; 
     component?: ComponentType<any> | keyof JSX.IntrinsicElements;
     to?: string;
+    href?: string;
 }
 
 
@@ -21,7 +22,8 @@ export const Button = ({
     size = 'md',
     className = '',
     disabled,
-    children
+    children,
+    ...rest
 }: ButtonProps) => {
     const buttonClass = classNames ({
         'button': true,
@@ -33,8 +35,7 @@ export const Button = ({
 
     const Component = component;
     return (
-        <div className="button-container">
-            <Component onClick={onClick} disabled={disabled} className={buttonClass}>
+            <Component onClick={onClick} disabled={disabled} className={buttonClass} {...rest}>
                 <svg className="button-corner" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15 15L9.53674e-07 -7.49338e-07L15 0L15 15ZM8.48214 6.51786L14.1071 6.51786L8.48214 0.892857L8.48214 6.51786Z" fill="currentColor"/>
                 </svg>
@@ -42,6 +43,5 @@ export const Button = ({
                     {children}
                 </div>
             </Component>
-        </div>
     )
 };
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,5 +1,15 @@
+import { Button } from "../component/button"
+import '../index.css'
+import '../styles/variables.css'
+import '../component/button.css'
+import { NavLink } from "react-router-dom"
+
 export const HomePage = () => {
     return (
-        <div></div>
+        <div>
+            <Button size="lg">Button Bigger</Button>
+            <Button>Button</Button>
+            <Button component={NavLink} size="sm" to="/shop">Go to Store</Button>
+        </div>
     )
 }
