8646f0485c6537d24491e7192c71eda6093825c8
Juan <juansobraoperez@gmail.com>
2025-04-24 12:08:46 +0200

    feature: css header background styles

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -6,21 +6,14 @@ import rupia from '../assets/rupia-icon.png'
 export const Header = ()=> {
     return (
         <header>
-            <span className="top-line-left"></span>
-            <span className="top-line-right"></span>
-            <span className="bottom-line-left"></span>
-            <span className="bottom-line-right"></span>
-
-            <span className="right-line-top"></span>
-            <span className="right-line-bottom"></span>
                 <div>
                     <NavLink className={"header-logo"} to="/">HYRULEPEDIA</NavLink>
                 </div>
                 <div className="header-content">
                     <nav className="header-nav">
-                        <NavLink to="/shop">Tienda</NavLink>
-                        <NavLink to="/collection">Colección</NavLink>
-                        <NavLink to="/profile">Perfil</NavLink>
+                        <NavLink to="/shop">STORE</NavLink>
+                        <NavLink to="/collection">COLLECTION</NavLink>
+                        <NavLink to="/profile">PROFILE</NavLink>
                     </nav>
                     <div className="user-money">
                         <p>1,000</p><img src={rupia} alt="" />  {/*habra que cambiarlo para que sea dinamico segun las rupias del usuario*/}
