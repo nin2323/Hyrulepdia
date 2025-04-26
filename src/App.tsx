7a72b3909fb3e04f672a1766e5d5996efd654f99
Aitana Sanchez <aitanabenrui2001@gmail.com>
2025-04-27 01:00:51 +0200

    style: margin top in StoreOpening component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-import { HyruleCard } from './components/hyrule-card/HyruleCard.tsx'
-import { StoreOpening } from './components/store-opening/StoreOpening.tsx'
 import { Header } from './components/header/header'
 import { Outlet } from 'react-router-dom'
 
