3b9427d221e6529ecfea8a2aaaf128256a979111
Gerard Bataller <gebaca01@gmail.com>
2025-05-13 13:41:42 +0200

    correciones profile

--- src/views/ProfilePage.tsx
+++ src/views/ProfilePage.tsx
@@ -1,5 +1,4 @@
 import { UserProfileEditor } from '../components/firebase/user-profile/UserProfileEditor';
-import { PoputContainer } from '../components/CardContainer/popup-container/poputContainer';
 
 //<UserProfileEditor />
 export const PorfilePage = () => {
@@ -7,14 +6,6 @@ export const PorfilePage = () => {
     <div>
       <h1>Perfil</h1>
       <UserProfileEditor />
-
-      <CardContainer colorClass='blue-theme' popUp={true}>
-        <h1>Contenido brillante</h1>
-      </CardContainer>
-
-      <CardContainer colorClass='golden-theme' popUp={false}>
-        <h1>Modo normal dorado</h1>
-      </CardContainer>
     </div>
   );
 };
