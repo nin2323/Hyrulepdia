449f6d8d5dd040d6545532659415d39e8e4fdb13
Juan <juansobraoperez@gmail.com>
2025-05-16 16:20:03 +0200

    feature: logic  cards user collection feature: css styles collection cover and discover feature: gif loading

--- src/services/getAllCards.ts
+++ src/services/getAllCards.ts
@@ -1,20 +1,20 @@
 import { HyruleCardType } from "../types/hyrule.types";
 
-export const getRandomRarity = () : 'common' | 'rare' | 'epic' => {
-    const rand = Math.random();
-    if(rand < 0.05) return 'epic'; //5%
-    if (rand < 0.30) return 'rare'; //25%
-    return 'common'; //70%
-};
+// export const getRandomRarity = () : 'common' | 'rare' | 'epic' => {
+//     const rand = Math.random();
+//     if(rand < 0.05) return 'epic'; //5%
+//     if (rand < 0.30) return 'rare'; //25%
+//     return 'common'; //70%
+// };
 
-//Esta función asigna puntos según la rareza
-export const getPointsByRarity = (rarity: 'common' | 'rare' | 'epic') : number => {
-    switch (rarity) {
-        case 'epic': return 100;
-        case 'rare': return 50;
-        case 'common': return 20;
-    }
-};
+// //Esta función asigna puntos según la rareza
+// export const getPointsByRarity = (rarity: 'common' | 'rare' | 'epic') : number => {
+//     switch (rarity) {
+//         case 'epic': return 100;
+//         case 'rare': return 50;
+//         case 'common': return 20;
+//     }
+// };
 
 
 export const getAllCards = async (): Promise<HyruleCardType[] | undefined> => {
@@ -29,7 +29,7 @@ export const getAllCards = async (): Promise<HyruleCardType[] | undefined> => {
         const allData = json.data;
 
         return allData.map((item: any) => {
-            const rarity = getRandomRarity();
+            // const rarity = getRandomRarity();
             const fullDescription = item.description;
             const trimmedDescription = item.description.length > 130
                 ? item.description.slice(0, 120) + '...'
@@ -44,8 +44,8 @@ export const getAllCards = async (): Promise<HyruleCardType[] | undefined> => {
                 location: item.common_locations?.join(', ') || "Unknown",
                 items: item.drops?.join(', ') || "None",
                 category: item.category,
-                rarity,
-                points: getPointsByRarity(rarity),
+                // rarity,
+                // points: getPointsByRarity(rarity),
             };
         });
     } catch (error: any) {
